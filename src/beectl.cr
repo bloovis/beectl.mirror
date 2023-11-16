349509919b363a67685eafaed6df7d747655e54d
marka@pobox.com <marka@pobox.com>
2023-11-16 12:54:38 +0000

    Reorganize as a module to get it in readiness for spec testing.
    FossilOrigin-Name: 3ba0665b3a4c58b136f56cba359d304fb96ce2bb2933c7e84e11b62b74f54d02

--- src/beectl.cr
+++ src/beectl.cr
@@ -8,113 +8,118 @@ require "json"
 
 DEBUG = false
 
-def dprint(s)
-  puts s if DEBUG
-end
+module Beectl
 
-def read_length
-  buf = Bytes.new(4)
-  STDIN.read(buf)
-  buf.each do |c|
-    dprint "c = #{c}"
-  end
-  len : Int32 = 0
-  [3,2,1,0].each do |i|
-    x : Int32 = buf[i].to_i32
-    len = (len << 8) + x
-  end
-  dprint(sprintf("%x\n", len))
-  return len
-end
+  extend self
 
-def read_request
-  len = read_length
-  slice = Bytes.new(len)
-  STDIN.read(slice)
-  jsonbuf = String.new(slice)
-  dprint "jsonbuf = #{jsonbuf}"
-  values = Hash(String, String | Array(String)).from_json(jsonbuf)
-  values.each do |k,v|
-    dprint "key = #{k}, value = #{v}, value type = #{typeof(v)}"
-    if k == "args"
-      a = v.as(Array(String))
-      dprint "v is an array of strings"
-      a.each {|s| dprint "arg: #{s}"}
+  def dprint(s)
+    puts s if DEBUG
+  end
+
+  def read_length(file : IO)
+    buf = Bytes.new(4)
+    file.read(buf)
+    buf.each do |c|
+      dprint "c = #{c}"
+    end
+    len : Int32 = 0
+    [3,2,1,0].each do |i|
+      x : Int32 = buf[i].to_i32
+      len = (len << 8) + x
+    end
+    dprint(sprintf("%x\n", len))
+    return len
+  end
+
+  def read_request(file : IO)
+    len = read_length(file)
+    slice = Bytes.new(len)
+    file.read(slice)
+    jsonbuf = String.new(slice)
+    dprint "jsonbuf = #{jsonbuf}"
+    values = Hash(String, String | Array(String)).from_json(jsonbuf)
+    values.each do |k,v|
+      dprint "key = #{k}, value = #{v}, value type = #{typeof(v)}"
+      if k == "args"
+	a = v.as(Array(String))
+	dprint "v is an array of strings"
+	a.each {|s| dprint "arg: #{s}"}
+      else
+	dprint "v is a string"
+      end
+    end
+    return values
+  end
+
+  def write_length(file : IO, len)
+    buf = Bytes.new(4)
+    [0,1,2,3].each do |i|
+      buf[i] = len.to_u8
+      len >>= 8
+    end
+    STDOUT.write(buf)
+  end
+
+  def write_response(file : IO, content : String)
+    response = JSON.build do |json|
+      json.object do
+	json.field "text", content
+      end
+    end
+
+    dprint "response = #{response}"
+    len = response.bytesize
+
+    write_length(file, len)
+    file << response
+  end
+
+  def main
+    # Read the JSON requestion, convert it to a hash and extract
+    # its values.
+    values = read_request(STDIN)
+    editor = values["editor"].as(String)
+    args = values["args"].as(Array(String))
+    ext = values["ext"].as(String)
+    text = values["text"].as(String)
+
+    # If a file suffix was specified, prepend it with a '.'; otherwise
+    # don't use a suffix.
+    if ext != ""
+      suffix = "." + ext
     else
-      dprint "v is a string"
+      suffix = nil
     end
-  end
-  return values
-end
 
-def write_length(len)
-  buf = Bytes.new(4)
-  [0,1,2,3].each do |i|
-    buf[i] = len.to_u8
-    len >>= 8
-  end
-  STDOUT.write(buf)
-end
+    # Create a temporary file and write the specified text to it.
+    tempfile = File.tempfile("beectl-", suffix)
+    temppath = tempfile.path
+    tempfile << text
+    tempfile.close
+    dprint "Created temp file #{temppath}"
 
-def write_response(content)
-  response = JSON.build do |json|
-    json.object do
-      json.field "text", content
+    # Construct the editor command and run it.
+    cmd = String.build do |cmd|
+      cmd << editor
+      args.each do |arg|
+	cmd << " " + arg
+      end
+      cmd << " " + temppath
     end
+    dprint "cmd = #{cmd}"
+    system(cmd)
+
+    # Read the contents of the temporary file and write it
+    # out in JSON formation as the response.
+    content = File.read(temppath)
+    dprint "Contents of tempfile:"
+    dprint content
+    write_response(STDOUT, content)
+
+    # Delete the temporary file.
+    dprint "Deleting #{temppath}"
+    tempfile.delete
   end
+end	# module Beectl
 
-  dprint "response = #{response}"
-  len = response.bytesize
-
-  write_length(len)
-  STDOUT << response
-end
-
-def main
-  # Read the JSON requestion, convert it to a hash and extract
-  # its values.
-  values = read_request
-  editor = values["editor"].as(String)
-  args = values["args"].as(Array(String))
-  ext = values["ext"].as(String)
-  text = values["text"].as(String)
-
-  # If a file suffix was specified, prepend it with a '.'; otherwise
-  # don't use a suffix.
-  if ext != ""
-    suffix = "." + ext
-  else
-    suffix = nil
-  end
-
-  # Create a temporary file and write the specified text to it.
-  tempfile = File.tempfile("beectl-", suffix)
-  temppath = tempfile.path
-  tempfile << text
-  tempfile.close
-  dprint "Created temp file #{temppath}"
-
-  # Construct the editor command and run it.
-  cmd = String.build do |cmd|
-    cmd << editor
-    args.each do |arg|
-      cmd << " " + arg
-    end
-    cmd << " " + temppath
-  end
-  dprint "cmd = #{cmd}"
-  system(cmd)
-
-  # Read the contents of the temporary file and write it
-  # out in JSON formation as the response.
-  content = File.read(temppath)
-  dprint "Contents of tempfile:"
-  dprint content
-  write_response(content)
-
-  # Delete the temporary file.
-  dprint "Deleting #{temppath}"
-  tempfile.delete
-end
-
-main
+Beectl.main
